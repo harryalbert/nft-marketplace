d167f9a760160b681cf8095c4a125b3e9a93c55e
harryalbert <harryalbert364@gmail.com>
2022-03-01 14:04:55 -0500

    fixed index bug

--- pages/index.js
+++ pages/index.js
@@ -18,8 +18,7 @@ export default function Home() {
 
   async function loadNFTs() {
     const provider = new ethers.providers.JsonRpcProvider();
-    console.log(nftmarketaddress);
-    const tokenContract = new ethers.Contract(nftaddress, NFT.abi.provider);
+    const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider);
     const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, provider);
     const data = await marketContract.fetchMarketItems();
 
